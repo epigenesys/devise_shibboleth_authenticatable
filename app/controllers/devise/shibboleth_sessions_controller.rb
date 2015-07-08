a9995aa08f2174cd39d9a52e47516eac84930df3
James Gregory <james.gregory@epigenesys.org.uk>
2015-07-08 11:23:56 +0100

    Sign in is now done via a protected callback method, and user is informed if Shibboleth is misconfigured at this point

--- app/controllers/devise/shibboleth_sessions_controller.rb
+++ app/controllers/devise/shibboleth_sessions_controller.rb
@@ -1,24 +1,17 @@
 class Devise::ShibbolethSessionsController < Devise::SessionsController
-  unloadable
-  
+  unless Rails.version =~/^4/
+    unloadable
+  end
+
   def new
+    # Redirect to the callback handler (which should be protected by Shibboleth via Apache/IIS/etc.)
+    return redirect_to action: 'callback'
+  end
 
-    if request.env['eppn'] && (request.env['gws_groups'] =~ /u_uwc4c/ )
-      authenticate_with_shibboleth request.env
-    end
+  def callback
+    return redirect_to action: 'misconfiguration', message: :session_missing if !request.env['Shib-Session-ID']
 
-    resource = resource_class.new(sign_in_params)
-
-    shib_config = YAML.load(ERB.new(File.read(::Devise.shibboleth_config || "#{Rails.root}/config/shibboleth.yml")).result)[Rails.env]
-
-    destination = request.protocol
-    destination << request.host
-    destination << ":#{request.port.to_s}" unless request.port == 80
-    destination << after_sign_in_path_for(resource)
-    
-    shib_login_url = shib_config['shibb_login_url'] 
-
-    redirect_to(shib_login_url)
+    redirect_to after_sign_in_path_for(warden.authenticate!(scope: resource_name))
   end
 
   def destroy
@@ -26,42 +19,8 @@ class Devise::ShibbolethSessionsController < Devise::SessionsController
     super
   end
 
-private
-  def authenticate_with_shibboleth(env)
-
-    resource = AdminUser.find_by_email(env['eppn'])
-
-    if (!resource.nil? && !Devise.shibboleth_create_user)
-      logger.warn("User(#{env['eppn']}) not found.  Not configured to create the user.")
-      return resource
-    end
-
-    c4c_group = ( env['gws_groups'] =~ /u_uwc4c/ )
-    logger.debug "in app controllers devise shibboleth_sessions_controller.rb line 39 of gem"
-    logger.debug c4c_group
-    logger.debug "is it nil?"
-    logger.debug c4c_group.nil?
-    return nil if c4c_group.nil?
-
-    if (c4c_group && resource.nil? && Devise.shibboleth_create_user)
-      logger.fatal("Creating user(#{env['eppn']}).")
-      resource = AdminUser.new()
-    end
-    return nil unless resource
-
-    save_user_shibboleth_headers(resource, env)
-
-    resource.save
-    resource
+  def misconfiguration
+    # Handles issues with misconfiguration (such as environment variables missing)
   end
 
-  def save_user_shibboleth_headers(user, env)
-    shib_config = YAML.load(ERB.new(File.read(::Devise.shibboleth_config || "#{Rails.root}/config/shibboleth.yml")).result)[Rails.env]
-    shib_config['user-mapping'].each do |model, header|
-      logger.info("Saving #{env[header]} to #{model}")
-      field = "#{model}="
-      value = env[header]
-      user.send(field, value.to_s)
-    end
-  end
 end
--- lib/devise_shibboleth_authenticatable/routes.rb
+++ lib/devise_shibboleth_authenticatable/routes.rb
@@ -3,8 +3,13 @@ ActionDispatch::Routing::Mapper.class_eval do
 
   def devise_shibboleth_authenticatable(mapping, controllers)
     resource :session, :only => [], :controller => controllers[:shibboleth_sessions], :path => "" do
-      get :new, :path => mapping.path_names[:sign_in], :as => "new"
-      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", via: [:get, :post]
+      sign_out_via = (Devise.respond_to?(:sign_out_via) && Devise.sign_out_via) || [:get, :post]
+
+      get   :new,               path: mapping.path_names[:sign_in],   as: "new"
+      match :destroy,           path: mapping.path_names[:sign_out],  as: "destroy",  via: sign_out_via
+
+      get   :callback,          path: '/shibboleth/callback',         as: 'callback'
+      get   :misconfiguration,  path: '/shibboleth/misconfiguration', as: 'misconfiguration'
     end
   end
 end
